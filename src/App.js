3657c1322f43fcf7112fcf7483819a68e37d4cb3
tttkhoa <khoakhukho228@gmail.com>
2023-02-01 18:35:53 +0700

    update component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
 import Header from './components/Header/header';
-import Search from "./components/Search/search";
 import Carousel from './components/Carousel/carousel';
-import News from "./components/News/news";
-import Schedule from "./components/Schedule/schedule";
+import ListMovie from './components/ListMovie/list-mosvie';
 import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <div>
       <Header />
-      <Search /> 
       <Carousel />
-      <News />
-      <Schedule />
+      <ListMovie />
       <Footer/>
     </div>
   );
---
+++ src/components/ListMovie/list-mosvie.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import "./list-movie.scss";
+
+export default class ListMovie extends Component {
+  render() {
+    return (
+      <div>ListMovie</div>
+    )
+  }
+}
