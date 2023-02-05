63af1585318354a6a4f4e47fa76bfd68cb46f320
tttkhoa <khoakhukho228@gmail.com>
2023-02-05 20:55:23 +0700

    update css header,carousel

--- src/pages/carousel.jsx
+++ src/pages/carousel.jsx
@@ -1,9 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import img1 from "../img/main-banner-01.jpg";
+import img2 from "../img/main-banner-02.jpg";
 
 export default class Carousel extends Component {
   render() {
     return (
-      <div>Carousel</div>
-    )
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-ride=""
+      >
+        <ol className="carousel-indicators">
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to={0}
+            className="active"
+          />
+          <li data-target="#carouselExampleIndicators" data-slide-to={1} />
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={img1} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={img2} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+    );
   }
 }
